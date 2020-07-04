a05fa374ea219e335772b35e5972a1f1c2e8b87a
Dhananjay Jadhav <dhananjayjadhav@2151>
2020-07-04 13:03:18 -0700

    dj

--- script.js
+++ script.js
@@ -8,13 +8,49 @@
 // username should not contain special characters
 // the function should return true if all the conditions above are validated.
 // return false if the validation fails
-
-// Progression 2 
+var submitbutton = document.getElementById("submit");
+submitbutton.addEventListener("click", checkUserName, checkEmail);
+submitbutton.addEventListener("click", checkEmail);
+submitbutton.addEventListener("click", checkPassword);
+function checkUserName() {
+    var specialcharacters = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-"
+        , "_", "+", "=", "{", "}", "[", "]", "|", "\\", "/", ":", ";", "\"", "\'",
+        "<", ">", ",", ".", "?"];
+    var set;
+    var uname = document.getElementById("usertext").value;
+    uname.trim();
+    for (var a = 0; a < uname.length; a++) {
+        if (specialcharacters.includes(uname[a]))
+            set = true;
+    }
+    if (uname.length >= 8 && uname.length < 15 && !uname.includes(" ") && !set)
+        return true;
+    else
+        return false;
+}
+// Progression 2
 // Check mail
 // create a function called as checkEmail()
 // only @ , . , _ [at,dot,underscore] is allowed
 // It can be alphanumeric in nature
 // return true if it passses all validation and false otherwise
+function checkEmail() {
+    var specialcharacters = ["~", "`", "!", "#", "$", "%", "^", "&", "*", "(", ")", "-"
+        , "+", "=", "{", "}", "[", "]", "|", "\\", "/", ":", ";", "\"", "\'",
+        "<", ">", ",", "?"];
+    var set;
+    var email = document.getElementById("emailtext").value;
+    email.trim();
+
+    for (var a = 0; a < email.length; a++) {
+        if (specialcharacters.includes(email[a]))
+            set = true;
+    }
+    if (!email.includes(" ") && !set)
+        return true;
+    else
+        return false;
+}
 
 // Progression 3
 // Check password
@@ -22,3 +58,32 @@
 // should be alphanumeric in nature
 // password must contain atleast one Uppercase, one number and special characters[!@#$%^&*()_]
 // return true else return false
+function checkPassword() {
+    var specialcharacters = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-"
+        , "_", "+", "=", "{", "}", "[", "]", "|", "\\", "/", ":", ";", "\"", "\'",
+        "<", ">", ",", ".", "?"];
+    var num = "1234567890";
+    var set = false, setcap = false, setnum = false, temp;
+    var pass = document.getElementById("passwordtext").value;
+    pass.trim();
+    for (var a = 0; a < pass.length; a++) {
+        if (specialcharacters.includes(pass[a]))
+            set = true;
+    }
+    for (var a = 0; a < pass.length; a++) {
+        temp = pass[a];
+        temp = temp.toUpperCase()
+        if (pass[a] == temp)
+            setcap = true;
+
+    }
+    for (var a = 0; a < pass.length; a++) {
+        if (num.includes(pass[a]))
+            setnum = true;
+    }
+
+    if (set && setcap && setnum)
+        return true;
+    else
+        return false;
+}
